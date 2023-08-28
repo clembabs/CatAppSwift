de2d3edfc135040355d574d6e99bb45ef6cc6770
clembabs <clembabs10@gmail.com>
2023-08-29 00:50:58 +0100

    Initial Commit

---
+++ cat_app/cat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  cat_appApp.swift
+//  cat_app
+//
+//  Created by Clement Babaniyi on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct cat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
